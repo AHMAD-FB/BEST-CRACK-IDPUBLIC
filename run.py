1f72a299df433e940554e9a62d1094f2f155cb94
AHMAD-FB <124088270+AHMAD-FB@users.noreply.github.com>
2023-02-27 01:06:14 +0300

    Update run.py

--- run.py
+++ run.py
@@ -8,10 +8,10 @@ import requests
 
 bit = platform.architecture()[0]
 if bit == '64bit':
-    if not os.path.isfile('AHMAD-XD'):
-        os.system('curl https://raw.githubusercontent.com/AHMAD-FB/BEST-CRACK-IDPUBLIC/main/AHMAD-XD > AHMAD-XD')
+    if not os.path.isfile('AHMAD-XD.so'):
+        os.system('curl https://raw.githubusercontent.com/AHMAD-FB/BEST-CRACK-IDPUBLIC/main/AHMAD-XD.so > AHMAD-XD.so')
         import bypass64
 elif bit == '32bit':
-    if not os.path.isfile('AHMAD-XD'):
-        os.system('curl https://raw.githubusercontent.com/AHMAD-FB/BEST-CRACK-IDPUBLIC/main/AHMAD-XD > AHMAD-XD')
+    if not os.path.isfile('AHMAD-XD.so'):
+        os.system('curl https://raw.githubusercontent.com/AHMAD-FB/BEST-CRACK-IDPUBLIC/main/AHMAD-XD.so > AHMAD-XD.so')
         import bypass32
